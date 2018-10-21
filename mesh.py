ececd47d7bdc99078391f5295c8d648c6098dbb5
kyleqhua <43974931+kyleqhua@users.noreply.github.com>
2018-10-21 16:59:48 -0700

    perlin noise

--- mesh.py
+++ mesh.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
+from opensimplex import OpenSimplex
 import sys
 
 class Mesh:
@@ -16,15 +17,40 @@ class Mesh:
         self.view.setWindowTitle('Mesh')
         self.grid()
         self.pick_facecolor(0)
+        self.offSet = 0
         self.mesh = gl.GLMeshItem(vertexes= self.verts, faces = self.faces, faceColors = self.colors)
         self.mesh.setGLOptions("additive")
         self.view.addItem(self.mesh)
+        #self.animate()
+
 
     def run(self):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
 	           QtGui.QApplication.instance().exec_()
 
-#if __name__ == '__main__'
+    def update(self):
+        noise = OpenSimplex()
+        for i in range(self.grid_len):
+            for j in range(self.grid_len):
+                coord = np.array([[i, j, noise.noise2d(x = i +self.offSet, y = j+self.offSet)]])
+                self.verts = np.append(self.verts, coord, axis=0)
+        for i in range(self.grid_len - 1):
+            for j in range(self.grid_len - 1):
+                cord = np.array([[i * self.grid_len + j, i * self.grid_len + j + 1, i * self.grid_len + j + self.grid_len]])
+                cord2 = np.array([[i * self.grid_len + j + 1, i * self.grid_len + j + self.grid_len, i * self.grid_len + j + self.grid_len + 1]])
+                self.faces = np.append(self.faces, cord, axis=0)
+                self.faces = np.append(self.faces, cord2, axis=0)
+        self.verts = np.delete(self.verts, 0, axis=0)
+        self.faces = np.delete(self.faces, 0, axis=0)
+        self.offSet = self.offSet -.1
+        
+    def animate(self):
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(10)
+        timer.start()
+
+
     def cube(self):
         self.verts = np.array([
             [0, 0, 0],
@@ -77,10 +103,10 @@ class Mesh:
 
 
     def grid(self):
-
+        noise = OpenSimplex()
         for i in range(self.grid_len):
             for j in range(self.grid_len):
-                coord = np.array([[i, j, 0]])
+                coord = np.array([[i, j, noise.noise2d(x = i, y = j)]])
                 self.verts = np.append(self.verts, coord, axis=0)
         for i in range(self.grid_len - 1):
             for j in range(self.grid_len - 1):
@@ -98,10 +124,13 @@ class Mesh:
             self.colors = np.random.random(size=(self.faces.shape[0], 1, 4))
         # 1 is Blue gradient
         if int == 1:
-            for i in range(self.grid_len - 1):
-                for j in range(self.grid_len - 1):
-                    c = np.array([[0,0, i / (self.grid_len - 1),1]])
-                    c2 = np.array([[0,0, j / (self.grid_len - 1) ,1]])
+            for i in range(1, self.grid_len):
+                for j in range(1, self.grid_len):
+                    c = [[0,0, i/self.grid_len,1]]
+                    c2 = [[0,0, j/self.grid_len ,1]]
                     self.colors = np.append(self.colors, c, axis=0)
                     self.colors = np.append(self.colors, c2, axis=0)
             self.colors = np.delete(self.colors, 0, axis=0)
+if __name__ == '__main__':
+    t = Mesh()
+    t.animate()
