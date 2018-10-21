c416e7c12217362c81dd6a331544289548ec6d69
kyleqhua <43974931+kyleqhua@users.noreply.github.com>
2018-10-21 15:17:10 -0700

    color update

--- mesh.py
+++ mesh.py
@@ -4,14 +4,19 @@ import pyqtgraph.opengl as gl
 import sys
 
 class Mesh:
+    verts = np.array([[0, 0, 0]])
+    faces = np.array([[0, 0, 0]])
+    colors = np.array([[0, 0, 0, 0]])
+    grid_len = 32
 
     def __init__(self):
         self.app =  QtGui.QApplication(sys.argv)
         self.view = gl.GLViewWidget()
         self.view.show()
         self.view.setWindowTitle('Mesh')
-        verts,faces = self.grid()
-        self.mesh = gl.GLMeshItem(vertexes= verts, faces = faces)
+        self.grid()
+        self.pick_facecolor(0)
+        self.mesh = gl.GLMeshItem(vertexes= self.verts, faces = self.faces, faceColors = self.colors)
         self.mesh.setGLOptions("additive")
         self.view.addItem(self.mesh)
 
@@ -19,9 +24,9 @@ class Mesh:
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
 	           QtGui.QApplication.instance().exec_()
 
-#if __name__ == '__main__':
+#if __name__ == '__main__'
     def cube(self):
-        verts = np.array([
+        self.verts = np.array([
             [0, 0, 0],
             [1, 0, 0],
             [0, 1, 0],
@@ -31,7 +36,7 @@ class Mesh:
             [1, 0, 1],
             [1, 1, 1]
         ])
-        faces = np.array([
+        self.faces = np.array([
             [0, 1, 4],
             [0, 2, 4],
             [0, 3, 6],
@@ -46,7 +51,7 @@ class Mesh:
             [7, 4, 2]
 
         ])
-        colors = np.array([
+        self.colors = np.array([
             [1, 0, 0, 0.3],
             [1, 0, 0, 0.3],
             [0, 1, 0, 0.3],
@@ -60,31 +65,43 @@ class Mesh:
             [1, 0, .66, 1],
             [1, 0, .3, 1],
         ])
-        return verts, faces, colors
+
     def some(self):
-        verts = np.empty((36, 3, 3), dtype=np.float32)
-        theta = np.linspace(0, 2 * np.pi, 37)[:-1]
-        verts[:, 0] = np.vstack([2 * np.cos(theta), 2 * np.sin(theta), [0] * 36]).T
-        verts[:, 1] = np.vstack([4 * np.cos(theta + 0.2), 4 * np.sin(theta + 0.2), [-1] * 36]).T
-        verts[:, 2] = np.vstack([4 * np.cos(theta - 0.2), 4 * np.sin(theta - 0.2), [1] * 36]).T
-
+        self.verts = np.empty((36, 3, 3), dtype=np.float32)
+        self.theta = np.linspace(0, 2 * np.pi, 37)[:-1]
+        self.verts[:, 0] = np.vstack([2 * np.cos(theta), 2 * np.sin(theta), [0] * 36]).T
+        self.verts[:, 1] = np.vstack([4 * np.cos(theta + 0.2), 4 * np.sin(theta + 0.2), [-1] * 36]).T
+        self.verts[:, 2] = np.vstack([4 * np.cos(theta - 0.2), 4 * np.sin(theta - 0.2), [1] * 36]).T
         ## Colors are specified per-vertex
-        colors = np.random.random(size=(verts.shape[0], 3, 4))
+        self.colors = np.random.random(size=(verts.shape[0], 3, 4))
 
-        return verts, theta, colors
 
     def grid(self):
-        verts = np.array([[0, 0, 0]])
-        faces = np.array([[0, 0, 0]])
-        grid_len = 32
-        for i in range(grid_len):
-            for j in range(grid_len):
+
+        for i in range(self.grid_len):
+            for j in range(self.grid_len):
                 coord = np.array([[i, j, 0]])
-                verts = np.append(verts, coord, axis=0)
-        for i in range(grid_len - 1):
-            for j in range(grid_len - 1):
-                cord = np.array([[i * grid_len + j, i * grid_len + 1, i * grid_len + j + grid_len]])
-                faces = np.append(faces, cord, axis=0)
-        verts = np.delete(verts, 0, axis=0)
-        faces = np.delete(faces, 0, axis=0)
-        return verts, faces
+                self.verts = np.append(self.verts, coord, axis=0)
+        for i in range(self.grid_len - 1):
+            for j in range(self.grid_len - 1):
+                cord = np.array([[i * self.grid_len + j, i * self.grid_len + j + 1, i * self.grid_len + j + self.grid_len]])
+                cord2 = np.array([[i * self.grid_len + j + 1, i * self.grid_len + j + self.grid_len, i * self.grid_len + j + self.grid_len + 1]])
+                self.faces = np.append(self.faces, cord, axis=0)
+                self.faces = np.append(self.faces, cord2, axis=0)
+        self.verts = np.delete(self.verts, 0, axis=0)
+        self.faces = np.delete(self.faces, 0, axis=0)
+
+
+    def pick_facecolor(self, int):
+        # 0 is random
+        if int == 0:
+            self.colors = np.random.random(size=(self.faces.shape[0], 1, 4))
+        # 1 is Blue gradient
+        if int == 1:
+            for i in range(self.grid_len - 1):
+                for j in range(self.grid_len - 1):
+                    c = np.array([[0,0, i / (self.grid_len - 1),1]])
+                    c2 = np.array([[0,0, j / (self.grid_len - 1) ,1]])
+                    self.colors = np.append(self.colors, c, axis=0)
+                    self.colors = np.append(self.colors, c2, axis=0)
+            self.colors = np.delete(self.colors, 0, axis=0)
--- mesh_test.py
+++ mesh_test.py
@@ -6,16 +6,26 @@ import sys
 
 def grid(grid_len):
     verts = np.array([[0,0,0]])
-
     faces = np.array([[0,0,0]])
+    face_2 = np.array([[0,0,0]])
     for i in range(grid_len):
         for j in range(grid_len):
             coord = np.array([[i, j, 0]])
             verts = np.append(verts, coord, axis = 0)
-    for i in range(grid_len - 1):
-        for j in range(grid_len - 1):
-            cord = np.array([[i * grid_len + j, i * grid_len + 1, i * grid_len + j + grid_len]])
+    for i in range(grid_len -1):
+        for j in range(grid_len -1):
+            cord = np.array([[i * grid_len + j, i * grid_len  + j + 1, i * grid_len + j + grid_len]])
+            cord2 = np.array([[i * grid_len  + j + 1,i * grid_len + j + grid_len,i * grid_len + j + grid_len+1]])
+            #print(cord, cord2)
             faces = np.append(faces, cord, axis = 0)
     verts = np.delete(verts, 0, axis =0)
     faces = np.delete(faces, 0, axis = 0)
-    return verts, faces
+
+    pcolors = np.random.random(size=(faces.shape[0], 1, 4))
+    print(pcolors[0])
+    print(len(pcolors), len(faces))
+    colors = np.array([[0, 0, 0, 0]])
+    for i in range(len(faces)):
+        c = np.array([[0, 0, 1, 1]])
+        colors = np.append(colors, c, axis=0)
+    colors = np.delete(colors, 0, axis=0)
