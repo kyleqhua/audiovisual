1f72af482ee975ba70056b8be5e8011ce94679ab
kyleqhua <43974931+kyleqhua@users.noreply.github.com>
2018-10-20 17:17:38 -0700

    grid changes

--- mesh.py
+++ mesh.py
@@ -74,14 +74,17 @@ class Mesh:
         return verts, theta, colors
 
     def grid(self):
-        verts = np.array([])
-        grid_len = 3
-        faces = np.array([])
+        verts = np.array([[0, 0, 0]])
+        faces = np.array([[0, 0, 0]])
+        grid_len = 32
         for i in range(grid_len):
             for j in range(grid_len):
-                verts = np.append([[i,j,0]], verts)
-        for i in range(grid_len-1):
-            for j in range(grid_len-1):
-                faces = np.append([[i*grid_len+j,i*grid_len+1, i*grid_len+j+grid_len]], faces)
-       
+                coord = np.array([[i, j, 0]])
+                verts = np.append(verts, coord, axis=0)
+        for i in range(grid_len - 1):
+            for j in range(grid_len - 1):
+                cord = np.array([[i * grid_len + j, i * grid_len + 1, i * grid_len + j + grid_len]])
+                faces = np.append(faces, cord, axis=0)
+        verts = np.delete(verts, 0, axis=0)
+        faces = np.delete(faces, 0, axis=0)
         return verts, faces
--- mesh_test.py
+++ mesh_test.py
@@ -5,16 +5,17 @@ import sys
 
 
 def grid(grid_len):
-    verts = np.array([])
+    verts = np.array([[0,0,0]])
 
-    faces = np.array([])
+    faces = np.array([[0,0,0]])
     for i in range(grid_len):
         for j in range(grid_len):
-            coord = [i, j, 0]
+            coord = np.array([[i, j, 0]])
             verts = np.append(verts, coord, axis = 0)
     for i in range(grid_len - 1):
         for j in range(grid_len - 1):
-            cord = [i * grid_len + j, i * grid_len + 1, i * grid_len + j + grid_len]
+            cord = np.array([[i * grid_len + j, i * grid_len + 1, i * grid_len + j + grid_len]])
             faces = np.append(faces, cord, axis = 0)
-
+    verts = np.delete(verts, 0, axis =0)
+    faces = np.delete(faces, 0, axis = 0)
     return verts, faces
